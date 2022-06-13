fdcf26b5270874eda9ce73b73df14d188a004be3
Besufekad_Terefe <besalu20@gmail.com>
2022-06-13 06:06:18 -0700

    half string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+0;10;1c#ifndef MAIN_H
 #define MAIN_H
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -8,4 +8,5 @@ int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 #endif
