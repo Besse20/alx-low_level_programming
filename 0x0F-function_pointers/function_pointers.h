5f5127a96918b2c7b46a95a8dfbc3996139db24b
Besufekad_Terefe <besalu20@gmail.com>
2022-07-06 02:23:42 -0700

    function that executes a function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNPOINTERS
 #define FUNPOINTERS
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 #endif
